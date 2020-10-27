8000f433aad1f22af95ecd59c8092540cad8f485
Aleksander Brymora <aleksander.australia@gmail.com>
2020-10-27 16:05:49 +1100

    refactored to nicer syntax and a class by nicer syntax i mean function chaining

--- src/atoms/pokemon.ts
+++ src/atoms/pokemon.ts
@@ -2,34 +2,45 @@ import { selector } from 'recoil';
 import { getAllPokemon, Pokemon } from '../db';
 import { filterState, MinMax } from './filters';
 
-const filterMinMax = (
-	all: Pokemon[],
-	stat: string,
-	{ min, max }: MinMax,
-): Pokemon[] => {
-	return all
-		.filter((p) => p[stat] < max.current)
-		.filter((p) => p[stat] > min.current);
-};
-
-// Object.defineProperty(Array.prototype, 'minMax', {
-// 	value: filterMinMax,
-// });
+class FilteredPokemon {
+	private pokemon: Pokemon[];
+	constructor() {
+		this.pokemon = getAllPokemon();
+	}
+	filterMinMax(stat: string, { min, max }: MinMax) {
+		this.pokemon = this.pokemon
+			.filter((p) => p[stat] < max.current)
+			.filter((p) => p[stat] > min.current);
+		return this;
+	}
+	filterByName(name: string) {
+		this.pokemon = this.pokemon.filter((p) =>
+			p.name.includes(name.toLowerCase()),
+		);
+		return this;
+	}
+	filterByType(type: string) {
+		this.pokemon = this.pokemon.filter(
+			(p) => p.types.filter((t) => t.includes(type.toLowerCase())).length > 0,
+		);
+		return this;
+	}
+	getFiltered(): Pokemon[] {
+		return this.pokemon;
+	}
+}
 
 export const pokemonState = selector({
 	key: 'pokemonState',
 	get: ({ get }) => {
 		const { experience, height, weight, name, type } = get(filterState);
-		const allPokemon = getAllPokemon();
-		let pokemon: Pokemon[] = [...allPokemon];
-		pokemon = filterMinMax(pokemon, 'experience', experience);
-		pokemon = filterMinMax(pokemon, 'height', height);
-		pokemon = filterMinMax(pokemon, 'weight', weight);
-		pokemon = pokemon
-			.filter((p) => p.name.includes(name.toLowerCase()))
-			.filter(
-				(p) => p.types.filter((t) => t.includes(type.toLowerCase())).length > 0,
-			);
-		return pokemon;
+		const pokemon = new FilteredPokemon();
+		return pokemon
+			.filterMinMax('experience', experience)
+			.filterMinMax('height', height)
+			.filterMinMax('weight', weight)
+			.filterByName(name)
+			.filterByType(type)
+			.getFiltered();
 	},
 });
